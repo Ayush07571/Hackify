c70095cb40c284c0dbfacec5d3f77ca9193b52bf
Ayush07571 <ayushkaran328@gmail.com>
2025-03-29 12:04:17 +0530

    chages restored

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import PasswordChallenge from './PasswordChallenge';
 import AttackSimulator from './AttackSimulator';
 import HackTheHacker from './HackTheHacker';
 import HackTheHackerComplete from './HackTheHackerComplete';
-import '../src/App.css';
+import './App.css';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
